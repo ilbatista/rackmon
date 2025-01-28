f73d1ec68f7d33a3195240ef35cdb062faa884ce
Ives Lima <ives.lb@gmail.com>
2025-01-28 15:19:47 -0400

    Iniciando o projeto

---
+++ rackmon.ino
@@ -0,0 +1,18 @@
+#include "PMS.h"
+#include "WiFi.h"
+#include "NTPClient.h"
+#include "WiFiUdp.h"
+#include "LiquidCrystal_I2C.h"
+#include "Wire.h"
+#include "RTClib.h"
+#include "DHTesp.h"
+#include "ThingSpeak.h"
+#include "Defines.h"
+
+void setup(){
+
+}
+
+void loop(){
+
+}
